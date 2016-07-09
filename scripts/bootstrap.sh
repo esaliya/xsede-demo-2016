a6dcf05fba8a72deede230794fb2f41ffdd24fe2
Saliya Ekanayake <esaliya@gmail.com>
2016-07-09 14:26:33 -0400

    adding bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -22,9 +22,13 @@ else
   unzip master.zip
   mv "$DEMO_NAME"-master $DEMO_NAME
   rm -rf master.zip
+  chmod +x $DEMO_NAME/scripts/*.sh
+
   sudo sed -i "1i 127.0.0.1 $HOSTNAME" /etc/hosts
   install_ifne pdsh
   sudo bash -c 'echo ssh > /etc/pdsh/rcmd_default'
   SCRIPT=~/$DEMO_NAME/scripts/setup_internal.sh
-  pdsh -w ^$NODE_FILE scp $MASTER:$SCRIPT ~/
+  pdsh -w ^$NODE_FILE scp -r $MASTER:~/$DEMO_NAME ~/
+#  pdsh -w ^$NODE_FILE $SCRIPT
+#  pdsh -w ^$NODE_FILE rm $SCRIPT
 fi
