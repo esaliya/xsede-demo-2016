5cbc8cf570bc9c1ad131148961bb9328dbb28358
Saliya Ekanayake <esaliya@gmail.com>
2016-07-10 02:03:02 -0400

    updating mmap run all

--- examples/damds/mmap.run.all.sh
+++ examples/damds/mmap.run.all.sh
@@ -8,4 +8,6 @@ $pd/mmap.run.generic.sh 1 8 $name xsede.2016.demo 1 $nodes $nodefile g $pd
 
 cd $pd/../../utils
 mvn install
+cp=$HOME/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:$HOME/.m2/repository/edu/indiana/soic/spidal/xsede/utils/1.0/utils-1.0.jar
 
+java -cp $cp edu.indiana.soic.spidal.xsede.utils.PvizBuilder $pd/damds-points.txt $HOME 5000 3 
